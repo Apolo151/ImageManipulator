bbfa1e2914dd6b9f3c5d759c968d812548ae4409
Apolo151 <abdogofor55@gmail.com>
2024-04-26 21:16:28 +0300

    add priority queue implementation

---
+++ ImageEncryptCompress/Compression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PriorityQueues;
+
+// Used priority queue implementation: https://github.com/mikkul/PriorityQueue/tree/master
+
+namespace ImageEncryptCompress
+{
+    public class Compression
+    {
+        public static PriorityQueues.BinaryPriorityQueue<RGBPixel> pq; // TODO: create with comparator
+        /// <summary>
+        /// Compress the image's 2D color array using huffman encoding
+        /// </summary>
+        /// <param name="image"> the 2d image array</param>
+        /// <returns>2D array of colors </returns>
+        public static RGBPixel[,] CompressImage(RGBPixel[,] image)
+        {
+            //TODO
+            return null;
+        }
+
+        /// <summary>
+        /// Decompress the image's 2D color array using reverse huffman encoding
+        /// </summary>
+        /// <param name="image"> the 2d image array</param>
+        /// <returns>2D array of colors </returns>
+        public static RGBPixel[,] DecompressImage(RGBPixel[,] image)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
---
+++ ImageEncryptCompress/Huffman.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+///Algorithms Project
+///Intelligent Scissors
+///
+
+namespace ImageEncryptCompress
+{
+    /// <summary>
+    /// Holds the pixel color in 3 byte values: red, green and blue
+    /// </summary>
+    public struct RGBPixel
+    {
+        public byte red, green, blue;
+    }
+    public struct RGBPixelD
+    {
+        public double red, green, blue;
+    }
+
+
+    /// <summary>
+    /// Library of static functions that deal with images
+    /// </summary>
+    public class DecodeEncode
+    {
+
+    }
+}
